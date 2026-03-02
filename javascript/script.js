994c677b874e5704bfe4b8aefbdee0f3846f8779
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-03-02 16:23:22 +0600

    HTMl completed

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+// const total=document.getElementById("total");
+// const interview=document.getElementById('interview');
+// const rejected=document.getElementById('rejected');
+// const allcardSection=document.getElementById("allCards");
+
+// function claculateCount(){
+//     total.innerText=allcardSection.children.length;
+// }
+
+
+
+let currentTab='All';
+
+function switchTab (tab){
+    console.log(tab);
+}
