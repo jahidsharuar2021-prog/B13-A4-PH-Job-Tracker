e1f58ec6440f19e8c59ac01a366e7686f9f9e068
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-03-07 16:14:06 +0600

    js file working

---
+++ script.js
@@ -0,0 +1,20 @@
+let currentTab="All";
+const tabActive=["bg-[#3B82F6]", "text-white","font-semibold"];
+const tabInactive=["bg-[#FFFFFF]","text-[#64748B]"];
+ function switchTab(tab){
+    console.log(tab);
+    const tabs=['All','Interview','Rejected'];
+    for(const t of tabs){
+     const tabName=document.getElementById(t+'-btn');
+     if(t==tab){
+        tabName.classList.add(...tabActive);
+        tabName.classList.remove(...tabInactive);
+     }
+     else{
+       tabName.classList.remove(...tabActive);
+        tabName.classList.add(...tabInactive); 
+     }
+    
+    }
+}
+
