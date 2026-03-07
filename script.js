827a6b55f82bb42b6f307d59241c9e1c3b34c6a3
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-03-08 02:19:51 +0600

    js section working

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let currentTab="All";
 const tabActive=["bg-[#3B82F6]", "text-white","font-semibold"];
 const tabInactive=["bg-[#FFFFFF]","text-[#64748B]"];
- function switchTab(tab){
-    console.log(tab);
+
+ //  three card select korlam
+    const allcardContainer=document.getElementById('allCard-container');
+    const interviewSection=document.getElementById('interview-section');
+    const rejectedSection=document.getElementById('rejected-section');
+  
+
+   // onclick button pabo
+  function switchTab(tab){
+    
     const tabs=['All','Interview','Rejected'];
-    for(const t of tabs){
+
+   //  array every element acces and tab ar tulona 
+  for(const t of tabs){
      const tabName=document.getElementById(t+'-btn');
      if(t==tab){
         tabName.classList.add(...tabActive);
@@ -16,6 +26,48 @@ const tabInactive=["bg-[#FFFFFF]","text-[#64748B]"];
      }
     
     }
+
+   //  all card hide 
+   allcardContainer.classList.add('hidden');
+   interviewSection.classList.add('hidden');
+   rejectedSection.classList.add('hidden');
+   
+   // //  every section hide 
+    if(tab==="All"){
+      allcardContainer.classList.remove('hidden');
+    }
+    else if(tab=='Interview'){
+     interviewSection.classList.remove('hidden');
+    }
+     else if(tab=='Rejected'){
+      rejectedSection.classList.remove('hidden');
+    }
 }
+
+// stat update section
+const totalStat=document.getElementById('total')
+const interviewStat=document.getElementById('interview')
+const rejectedStat=document.getElementById('rejected')
+console.log(totalStat,interviewStat,rejectedStat);
+
+// id gula dorbo and update korbo
+totalStat.innerText=allcardContainer.children.length;
+
+
 switchTab(currentTab);
 
+//  document.getElementById('allCard-container').addEventListener("click",function(event){
+//    console.log(event.target);
+// })
+
+
+const allCardContainer = document.getElementById('allCard-container');
+
+allcardContainer.addEventListener("click", function(event){
+    console.log("event.target:", event.target);
+    console.log("event.currentTarget:", event.currentTarget);
+    const card = event.target.closest('section[class^="card-number"]');
+    if(card){
+        console.log("Card clicked:", card.className);
+    }
+});
