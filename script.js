8da307b9eb40ad93aed3a064d28a574cf849511d
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-03-08 16:26:44 +0600

    finally completed my assignment-4

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ const tabInactive=["bg-[#FFFFFF]","text-[#64748B]"];
     const allcardContainer=document.getElementById('allCard-container');
     const interviewSection=document.getElementById('interview-section');
     const rejectedSection=document.getElementById('rejected-section');
-    console.log(interviewSection);
-  
+    const emptyslide=document.getElementById('emty-slide');
+    
 
    // onclick button pabo
   function switchTab(tab){
-    
-    const tabs=['All','Interview','Rejected'];
+  const tabs=['All','Interview','Rejected'];
+   currentTab=tab;
 
    //  array every element acces and tab ar tulona 
   for(const t of tabs){
@@ -32,47 +32,83 @@ const tabInactive=["bg-[#FFFFFF]","text-[#64748B]"];
    allcardContainer.classList.add('hidden');
    interviewSection.classList.add('hidden');
    rejectedSection.classList.add('hidden');
+    emptyslide.classList.add('hidden');
    
-   // //  every section hide 
+    //  showing section depend condition
     if(tab==="All"){
       allcardContainer.classList.remove('hidden');
+      if(allcardContainer.children.length<1){
+        emptyslide.classList.remove('hidden');
+      }
     }
     else if(tab=='Interview'){
      interviewSection.classList.remove('hidden');
+     if(interviewSection.children.length<1){
+      emptyslide.classList.remove('hidden');
+     }
     }
      else if(tab=='Rejected'){
       rejectedSection.classList.remove('hidden');
+      if(rejectedSection.children.length<1){
+        emptyslide.classList.remove('hidden');
+      }
     }
-}
+     updatestat();
+};
 
-// stat update section
-const totalStat=document.getElementById('total')
-const interviewStat=document.getElementById('interview')
-const rejectedStat=document.getElementById('rejected')
-console.log(totalStat,interviewStat,rejectedStat);
+// state update section
+const totalStat=document.getElementById('total');
+const interviewStat=document.getElementById('interview');
+const rejectedStat=document.getElementById('rejected');
+const avaibleState=document.getElementById('avaible-state');
 
-// id gula dorbo and update korbo
-totalStat.innerText=allcardContainer.children.length;
-
-
-switchTab(currentTab);
-
+ switchTab(currentTab);
  document.getElementById('allCard-container').addEventListener("click",function(event){
 const clickElement=event.target;
 const card = clickElement.closest(".card");
+const parent=card.parentNode;
+const status=card.querySelector('.status');
 
+// Logical part for if condition
 if(clickElement.classList.contains('interview-cardbtn')){
   interviewSection.appendChild(card);
+  status.innerText='Interviewed';
+  updatestat();
 }
 if(clickElement.classList.contains('rejected-cardbtn')){
   rejectedSection.appendChild(card);
+  status.innerText='Rejected';
+  updatestat();
 }
 if(clickElement.classList.contains('delete')){
-  // console.log('delete btn clicked');
+  parent.removeChild(card);
+  updatestat();
 }
+}
+);
 
+// status update
+function updatestat(){
+  const counts={
+    All:allcardContainer.children.length,
+    Interview:interviewSection.children.length,
+    Rejected:rejectedSection.children.length,
+  }
+  totalStat.innerText=counts.All;
+  interviewStat.innerText=counts.Interview;
+  rejectedStat.innerText=counts.Rejected;
+  avaibleState.innerText=counts[currentTab];
+
+  if(counts[currentTab]<1){
+     emptyslide.classList.remove('hidden');
+  }
+  else{
+     emptyslide.classList.add('hidden');
+  }
+
+ 
 }
-)
+ updatestat();
 
 
 
